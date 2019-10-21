8c492373005903cea4399547046e6ce0b492b73d
klwaamzhang <36008792+klwaamzhang@users.noreply.github.com>
2019-10-21 11:52:12 -0400

    Get Date and put date to tvDateHome

--- app/src/main/java/com/example/isitraining/HomeActivity.java
+++ app/src/main/java/com/example/isitraining/HomeActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,9 +18,17 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // set toolbar title
         Toolbar tbHome = findViewById(R.id.tbHome);
         setSupportActionBar(tbHome);
         getSupportActionBar().setTitle("Local Weather");
+
+        // get Date and put date to tvDateHome
+        TextView tvDateHome = findViewById(R.id.tvDateHome);
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd | EEE");
+        tvDateHome.setText(dateFormat.format(cal.getTime()));
+
     }
 
     // add menu to toolbar
