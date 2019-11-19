5c75a9de4377d51aa0fff480ddeb3abf04fe34f9
klwaamzhang <36008792+klwaamzhang@users.noreply.github.com>
2019-11-18 19:34:39 -0500

    User could logout now

--- app/src/main/java/com/example/isitraining/AccountActivity.java
+++ app/src/main/java/com/example/isitraining/AccountActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AccountActivity extends AppCompatActivity {
@@ -17,11 +20,35 @@ public class AccountActivity extends AppCompatActivity {
         // set toolbar title
         Toolbar tbAccount = findViewById(R.id.tbAccount);
         setSupportActionBar(tbAccount);
-        getSupportActionBar().setTitle("Update User Account Information");
+        getSupportActionBar().setTitle("Update User Information");
     }
 
     public void goToFeedbackPage(View view){
         Intent intentFeedback = new Intent(this,FeedbackActivity.class);
         startActivity(intentFeedback);
+    }
+
+    // add menu to toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menu_toobar_logout, menu);
+
+        return true;
+    }
+
+    // menu item onclick to different activity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.goToHomePageWithoutLogin:
+                Intent intentHome = new Intent(this,HomeActivity.class);
+                startActivity(intentHome);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
