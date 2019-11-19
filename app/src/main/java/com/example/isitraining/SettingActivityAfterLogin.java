fdd6b87fa46c6ec66edb9705c6a1d1a3e10ff0d8
klwaamzhang <36008792+klwaamzhang@users.noreply.github.com>
2019-11-18 20:26:39 -0500

    Finish UI for Registered User setting

--- app/src/main/java/com/example/isitraining/SettingActivityAfterLogin.java
+++ app/src/main/java/com/example/isitraining/SettingActivityAfterLogin.java
@@ -1,11 +1,17 @@
 package com.example.isitraining;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
-public class SettingActivityAfterLogin extends AppCompatActivity {
+public class SettingActivityAfterLogin extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,27 @@ public class SettingActivityAfterLogin extends AppCompatActivity {
         Toolbar tbSetAfterLogin = findViewById(R.id.tbSetAfterLogin);
         setSupportActionBar(tbSetAfterLogin);
         getSupportActionBar().setTitle("Setting(After Login)");
+
+        Button btnOpenTimePicker = findViewById(R.id.btnOpenTimePicker);
+
+        btnOpenTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show((getSupportFragmentManager()), "time picker");
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+        
+        TextView tvNotiTime = findViewById(R.id.tvNotiTime);
+        if(minute < 10){
+            tvNotiTime.setText("Will Notify You At " + hour + ":0" + minute);
+        }else {
+            tvNotiTime.setText("Will Notify You At " + hour + ":" + minute);
+        }
+
     }
 }
